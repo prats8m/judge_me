ff67eeffd7949dab82b547b85b82f1098942f9bb
prateek singh <prateek.singh@vvdntech.com>
2017-10-25 22:10:59 +0530

    first commit

---
+++ application/controllers/main.php
@@ -0,0 +1,135 @@
+<?php
+error_reporting(0);
+/**
+ *
+ */
+class Main extends CI_Controller
+{
+
+  function __construct()
+  {
+  parent::__construct();
+  $this->load->library('session');
+  $this->load->database();
+  $this->load->model('main_model','mm');
+  $this->load->helper('url', 'form');
+  }
+
+  public function create_link(){
+  $data = file_get_contents("php://input"); // accept json
+  $user_data = json_decode($data, true); // decode json
+  $user_data['name'] = str_replace("%20"," ",$user_data['name']);
+  $username = $user_data['name'].rand(999,9999);
+  $insertData = array(
+    'user_name' =>$username,
+    'name'=>$user_data['name']
+  );
+  $link  = "http://localhost/judge_me/index.php/main/quiz/".$username."/".$user_data['name'];
+  $response = $this->mm->insert_user($insertData);
+  $this->mm->send_response(true,"Success",$link);
+  }
+
+   public function set_session(){
+  $data = file_get_contents("php://input"); // accept json
+  $user_data = json_decode($data, true); // decode json
+  $user_data['name'] = str_replace("%20"," ",$user_data['name']);
+  $username = $user_data['name'].rand(999,9999);
+  $insertData = array(
+    'user_name' =>$username,
+    'name'=>$user_data['name']
+  );
+  $response = $this->mm->insert_user($insertData);
+  $this->mm->send_response(true,"Success",$response);
+  }
+
+
+  public function is_logged_in()
+  {
+    $is_logged_in=$this->session->all_userdata();
+    if($is_logged_in['id']){
+      $this->mm->send_response(true,"Success",$is_logged_in['name']);
+    }
+    else{
+      $this->mm->send_response(true,"Failure",null);
+    }
+  }
+
+
+  public function is_form_submited(){
+    $is_logged_in=$this->session->all_userdata();
+    if($is_logged_in['submit']){
+      $this->mm->send_response(true,"Success",null);
+    }
+    else{
+      $this->mm->send_response(true,"Failure",null);
+    }
+  }
+
+  public function logout()
+  {
+    $this->session->sess_destroy();
+    $this->mm->send_response(true,"Success",null);
+  }
+
+
+
+  public function list_questions($offset){
+    $count = 0;
+    $where  = array('status'=>1);
+    $limit=1;
+    $Qresponse = $this->mm->list_questions($where,$limit,$offset);
+    $where  = array('status'=>1,'qid'=>$Qresponse[0]['id']);
+    $Oresponse = $this->mm->list_options($where);
+    $response['question']=$Qresponse[0];
+    $response['option']=$Oresponse;
+    
+    $this->mm->send_response(true,"Success",$response);
+
+  }
+
+  public function save_response(){
+  $is_logged_in=$this->session->all_userdata();
+  $data = json_decode(file_get_contents("php://input"),true); // accept json
+  $user_id = $is_logged_in['id'];
+    $insert_data= array(
+      'user_id'=> $user_id ,
+      'friend_id'=> $data['fid'],
+      'question_id'=> $data['qid'],
+      'answer_id'=> $data['aid'],
+
+    );
+   $this->mm->save_response($insert_data);
+    $this->mm->send_response(true,"Success",nul);
+  }
+
+public function quiz($username,$name){
+  $where = array('user_name'=>$username);
+  $data = $this->mm->select_user($where);
+  $user_id = $data[0]['id'];
+  $this->session->set_userdata('id',$user_id);
+  $this->session->set_userdata('username',$username);
+  $this->session->set_userdata('name',$name);
+  header('Location:http://localhost/judgeMe/#!');
+  }
+
+
+  public function list_response(){
+    $is_logged_in=$this->session->all_userdata();
+    $user_id = $is_logged_in['id'];
+    if($user_id)
+    $data2 = $this->mm->select_response($user_id);
+      $this->mm->send_response(true,"Success",$data2);
+  
+  }
+
+  public function single_response($fid){
+  $is_logged_in=$this->session->all_userdata();
+  $data = json_decode(file_get_contents("php://input"),true); // accept json
+  $uid = $is_logged_in['id'];
+  $data = $this->mm->select_all_response($uid,$fid);
+   $this->mm->send_response(true,"Success",$data);
+  }
+
+}
+
+ ?>
---
+++ application/models/main_model.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+*
+*/
+class Main_model extends CI_Model
+{
+    public function send_response($status,$msg,$data){
+    $response['status']=$status;
+    $response['msg']=$msg;
+    $response['data']=$data;
+    echo json_encode($response); // encode json
+    die();
+
+  }
+
+
+  public function select_session($where){
+  $query= $this->db->get_where('ci_sessions', $where)->result_array();
+  return $query;
+}
+
+
+ public function select_user($where){
+  $query= $this->db->get_where('user', $where)->result_array();
+  return $query;
+}
+
+  public function list_questions($where,$limit,$offset){
+    $this->db->limit($limit,$offset);
+  $query= $this->db->get_where('questions',$where)->result_array();
+  return $query;
+}
+
+
+ public function list_options($where){
+  $query= $this->db->get_where('options',$where)->result_array();
+  return $query;
+}
+
+ public function insert_user($insert_data){
+ $this->db->insert('user', $insert_data);
+ return $this->db->insert_id(); 
+}
+
+ public function save_response($insert_data){
+ $this->db->insert('response', $insert_data); 
+ }
+
+ public function select_response($user_id){
+ $SQL = "SELECT DISTINCT `friend_id`,`name` FROM response INNER JOIN user ON response.friend_id = user.id WHERE response.user_id =".$user_id."";
+ $query = $this->db->query($SQL);
+//  echo $this->db->last_query();
+return $query->result_array(); 
+ }
+ 
+
+
+ public function select_all_response($uid,$fid){
+ $SQL = "SELECT questions.question,options.option FROM response 
+        INNER JOIN questions ON response.question_id = questions.id
+        INNER JOIN options ON response.answer_id = options.id 
+        WHERE response.user_id =".$uid." AND response.friend_id =".$fid."";
+ $query = $this->db->query($SQL);
+//  echo $this->db->last_query();
+return $query->result_array(); 
+ }
+}
+
+?>
